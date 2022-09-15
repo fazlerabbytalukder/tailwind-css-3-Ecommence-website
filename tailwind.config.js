f4c0bac6d5c68faf61d5f245ddec2159a203fc4d
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2022-09-14 23:18:35 -0700

    setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ["./dist/**/*.html"],
+  theme: {
+    screens: {
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px'
+    },
+    container: {
+      center: true,
+      padding:'1rem'
+    },
+    extend: {
+      fontFamily: {
+        poppins: "'Poppins', sans-serif",
+        roboto:"'Roboto', sans-serif",
+      },
+      colors: {
+        'primary':'#FD3D57'
+      }
+    },
+  },
+  plugins: [],
+}
